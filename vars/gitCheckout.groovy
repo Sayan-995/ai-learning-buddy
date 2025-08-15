b70c8c173554d1332552b9b966af5293188785fe
sayan <sayandutta995@gmail.com>
2025-08-16 01:02:57 +0530

    fixed syntax error

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,5 +1,5 @@
 def call(String giturl,String gitBranch,String credentialsId){
-    git url: "${giturl}"
-    branch: "${gitBranch}"
+    git url: "${giturl}",
+    branch: "${gitBranch}",
     credentialsId: "${credentialsId}"
 }
